ebc94e20dd0fd5f7d5dc9f4a6aa148b166afcf25
Soren Hansen <Soren.Hansen@ril.com>
2014-01-23 15:13:00 +0530

    Add Om

--- manifests/init.pp
+++ manifests/init.pp
@@ -18,4 +18,9 @@ class rjil {
     realname => 'Sanjay S Upadhyay',
     sshkeys  => "ssh-dss AAAAB3NzaC1kc3MAAACBAI+y8C9f48PXOo6DOUgvDdyDlEmaiRyaHep64ddLY/OLu5KPLb0Qr8NVQ/rlU8lvLLzTmgj8LfFPrzt7zHuNpW4OVxcjZa+vAsFVCVKOfwHS3TatPXClHGKqp1W797HqDtEViti6rpHbxsPUkYb3uqPTlt3Hb/8YCTnfuRQGffixAAAAFQCE8EleKsutjOQbrLIjzQx2OGOgqwAAAIBMkXFXlaEGff9VUd4E4K3S6FhMpRjJNImVhHzSfm/xBAPTLmb6fJsE1B1EhMa9drEUj3EYAbbRWsEXwOqsVfbxlUbL8pALi+HasKnJ3IZ5tgpHOGpARNgtfvGppyr7yOdRWuG/4UOjgpjlrVamaCn/F8d5SJhXcfJIfiPbp7UCGAAAAIB4La5onWCklzSRY8RFoOs6mVX3KhS+MsOf/xd4C2R9U63bUejP56HHF+enGdbHcwOHPp9/DAedgP+bd0rd9D/aJoUfJUE0r3U/r6/WyrCZsChxQ0E2ibeHnyNrErqxZYtrkXzESmakpMSwfPT6CoGO7q4NEailFgnH5WU+EeT9xg== sanjayu@glowfriend.com\nssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAqqn1SeVuzQIZTski6aeWhH39E7icc35Axb5Hv9E8JXxXmxCIgOk+N9bEVPBB7UQsb7R7gLsbYraeKHNFg9D2n9YPEcUVdBttON2Th5tg/y4nMOxzkoWWLIe9HdrovfpAE+RzA6Ti28zjhddNCsL9ZU5riCJzdI1sXlJD5OmjYMuI+mocAzwh8dHrMrXD5iqzfQoK7FC1XzMuOTwa+so4tZqgeqWStaGgTAfTXmYi/0RIeFG2OXc03khBzLP/gLprlFEHJX4O60gGLEHbfyRk11x+trR9JUobR1FFF0q8JNoeuRW4bKkelCtEGjDry+Qh4yIg4cbBGunlRf3a4VWDqw== sanjayu@glowfriend.com\n",
   }
+
+  @rjil::localuser { 'omp':
+    realname => 'Om Prakash Pandey',
+    sshkeys  => "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAnyfDtofClb1cyEtv/osg+zxhBLbpuQSPXdMjf+KUoC8KxvDl8+yZRC6tY0wzTCX5deNdvaZb02FbHFuqKg1HEW3QNnvobkc/jl4PgYbXoCZe4e+sLB8szMHHrCgrQH07HMvwiQKOJSlmrR8GVKTHlDpGC1wbv9s/udy1hOweHDp1mXYyFPAq1tvENs64S+LAojtmaGjSrwQLlgfc3t56e9+C52+vu+RmwNRA6o07kZl+L3DUyl4OtX2q08pLH2LJ7hso/jqHPN7ozeG77tNgwWVp1O6oO443LFMA/+1nVlZFhJjwNcIeKbKV+eAAFWmA6l5xt75L27tS0Jm1Si0kww==\n",
+  }
 }
