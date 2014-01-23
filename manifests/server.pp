9eae05fe579fb17f5ef187eba43277278ce9d21f
Soren Hansen <Soren.Hansen@ril.com>
2014-01-23 17:05:14 +0530

    Make members of sudo able to actually sudo

--- manifests/server.pp
+++ manifests/server.pp
@@ -9,6 +9,15 @@ class rjil::server() {
     value => "no"
   }
 
+  file { '/etc/sudoers':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0440',
+    source  => 'puppet:///modules/rjil/sudoers',
+    replace => true,
+  }
+
   realize (
     Rjil::Localuser['soren'],
   )
