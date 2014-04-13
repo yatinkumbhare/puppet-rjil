a3992b2a78bf30cb5ae140a4c65febbcaf590b3d
Soren Hansen <Soren.Hansen@ril.com>
2014-04-13 22:51:00 +0530

    Add beginnings of a controller class

---
+++ manifests/jiocloud/controller.pp
@@ -0,0 +1,15 @@
+class rjil::jiocloud::controller(
+  $keystone_db_password => "sample_keystone_db_password") {
+
+  class { 'mysql::server': }
+  class { 'keystone::db::mysql':
+    password      => $keystone_db_password,
+    allowed_hosts => '%',
+  }
+  class { 'keystone':
+    verbose        => True,
+    catalog_type   => 'sql',
+    admin_token    => 'random_uuid',
+    sql_connection => "mysql://keystone_admin:$keystone_db_password@localhost/keystone",
+  }
+}
