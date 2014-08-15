619c4ba9f314c708a807c3d58bc3fc6990cf8bd6
Soren Hansen <Soren.Hansen@ril.com>
2014-08-15 14:52:38 +0200

    Make etcd listen everywhere

--- manifests/jiocloud/etcd.pp
+++ manifests/jiocloud/etcd.pp
@@ -1,9 +1,9 @@
 class rjil::jiocloud::etcd() {
   class { '::etcd':
     addr           => "${::ipaddress}:4001",
-    bind_addr      => "${::ipaddress}:4001",
+    bind_addr      => "0.0.0.0:4001",
     peer_addr      => "${::ipaddress}:7001",
-    peer_bind_addr => "${::ipaddress}:7001",
+    peer_bind_addr => "0.0.0.0:7001",
 
   }
 }
