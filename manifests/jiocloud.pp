3c422f4a9a072471d3029df301d2d77b4d5f7547
Soren Hansen <Soren.Hansen@ril.com>
2014-08-15 23:25:51 +0200

    State dependency chain between script, job and service

--- manifests/jiocloud.pp
+++ manifests/jiocloud.pp
@@ -11,15 +11,13 @@ class rjil::jiocloud {
     mode => '0755',
     owner => 'root',
     group => 'root'
-  }
-
+  } ->
   file { '/etc/init/poll-upgrade.conf':
     source => 'puppet:///modules/rjil/poll-upgrade.upstart',
     mode => '0644',
     owner => 'root',
     group => 'root'
-  }
-
+  } -> 
   service { 'poll-upgrade':
     ensure   => 'running',
     enable   => true,
