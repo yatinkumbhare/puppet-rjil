25190d4ec72ea75498ec6afcffc14c0450a43496
Soren Hansen <Soren.Hansen@ril.com>
2014-08-15 21:57:51 +0200

    Add script to poll etcd for upgrades

---
+++ files/poll-upgrade.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+do_upgrade {
+	pending_version=$1
+	etcdctl set upgrade_running/$(hostname) true
+	export http_proxy="http://10.135.121.138:3128/"
+	export https_proxy="http://10.135.121.138:3128/"
+	export no_proxy="127.0.0.1,localhost,10.135.127.35"
+	apt-get update
+	apt-get dist-upgrade -o Dpkg::Options::="--force-confnew" -y
+	puppet apply /etc/puppet/manifests/site.pp
+	etcdctl rm upgrade_running/$(hostname)
+	etcdctl rmdir upgrade_running || true
+	echo $pending_version > /etc/running_version
+}
+
+pending_version=$(etcdctl get current_version)
+
+while true
+do
+	if [ ! -e /etc/running_version ]
+	then
+		do_upgrade $pending_version
+	else
+		read running_version < /etc/running_version
+		if  [ ${pending_version} -gt ${running_version} ]
+		then
+			do_upgrade $pending_version
+		fi
+	fi
+	read running_version < /etc/running_version
+	etcdctl set running_version/${running_version} $(hostname) --ttl 60
+	timeout 30 etcdctl watch current_version || true
+done
--- manifests/jiocloud.pp
+++ manifests/jiocloud.pp
@@ -5,4 +5,24 @@ class rjil::jiocloud {
     owner => 'root',
     group => 'root'
   }
+
+  file { '/usr/local/bin/poll-upgrade.sh':
+    source => 'puppet:///modules/rjil/poll-upgrade.sh',
+    mode => '0755',
+    owner => 'root',
+    group => 'root'
+  }
+
+  file { '/etc/init/poll-upgrade.conf':
+    source => 'puppet:///modules/rjil/poll-upgrade.upstart',
+    mode => '0644',
+    owner => 'root',
+    group => 'root'
+  }
+
+  service { 'poll-upgrade':
+    ensure   => 'running',
+    enable   => true,
+    provider => 'upstart',
+  }
 }
