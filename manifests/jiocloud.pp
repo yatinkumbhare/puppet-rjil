b6d9a4f22649f036f305c1b3aff0da4f8986edbf
Soren Hansen <Soren.Hansen@ril.com>
2014-08-31 20:36:07 +0530

    Remove resources that are no longer used

--- manifests/jiocloud.pp
+++ manifests/jiocloud.pp
@@ -8,20 +8,6 @@ class rjil::jiocloud {
 
   package { 'python-jiocloud': }
 
-  file { '/usr/local/bin/poll-upgrade.sh':
-    ensure => absent
-  }
-
-  file { '/etc/init/poll-upgrade.conf':
-    ensure => absent
-  }
-
-  service { 'poll-upgrade':
-    ensure   => 'stopped',
-    enable   => false,
-    provider => 'upstart',
-  }
-
   file { '/usr/local/bin/maybe-upgrade.sh':
     source => 'puppet:///modules/rjil/maybe-upgrade.sh',
     mode => '0755',
