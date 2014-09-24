148791c36f08fed6e057ec40c8c494a7a817d634
Harish Kumar <hkumarmk@gmail.com>
2014-09-24 05:34:57 +0000

    Added haproxy for contrail
    1. Added rjil::haproxy::contrail to load balance contrail api services
    2. Changed rjil::haproxy_service to support different listen and balancer ports
    3. Added spec tests.
    
    Changed lb-stats port of haproxy to 8094 as the port 8084 is used by contrail.

--- manifests/haproxy_service.pp
+++ manifests/haproxy_service.pp
@@ -1,8 +1,13 @@
+#
+# Define: rjil::haproxy_service
+#
+
 define rjil::haproxy_service(
-  $vip = '0.0.0.0',
-  $balancer_ports = [],
+  $vip               = '0.0.0.0',
+  $balancer_ports    = [],
+  $listen_ports      = [],
   $cluster_addresses = [],
-  $listen_options   =  {
+  $listen_options    =  {
     'option'  => 'tcpka',
     'option'  => 'abortonclose',
     'balance' => 'roundrobin',
@@ -14,19 +19,42 @@ define rjil::haproxy_service(
 ) {
 
   if $cluster_addresses != [] {
+    ## Listen ports can be different from balancer ports
+    ## This is applicable for contrail servers
+    ## So enabling a way to provide both the ports if required
 
-    haproxy::listen { $name:
+    if $listen_ports == [] {
+      if $balancer_ports  != [] {
+        $listen_ports_orig = $balancer_ports
+      } else {
+        fail('Either balancer_ports or listen_ports must be provided')
+      }
+    } else {
+      $listen_ports_orig = $listen_ports
+    }
+
+    if $balancer_ports == [] {
+      if $listen_ports  != [] {
+        $balancer_ports_orig = $listen_ports
+      } else {
+        fail('Either balancer_ports or listen_ports must be provided')
+      }
+    } else {
+      $balancer_ports_orig = $balancer_ports
+    }
+
+    ::haproxy::listen { $name:
       ipaddress        => $vip,
-      ports            => $balancer_ports,
+      ports            => $listen_ports_orig,
       mode             => $listen_mode,
       collect_exported => false,
       options          => $listen_options,
       bind_options     => $bind_options
     }
 
-    haproxy::balancermember { $name:
+    ::haproxy::balancermember { $name:
       listening_service => $name,
-      ports             => $balancer_ports,
+      ports             => $balancer_ports_orig,
       server_names      => $cluster_addresses,
       ipaddresses       => $cluster_addresses,
       options           => $balancer_options,
---
+++ spec/defines/haproxy_service_spec.rb
@@ -0,0 +1,102 @@
+require 'spec_helper'
+
+describe 'rjil::haproxy_service' do
+  let (:facts) { {
+    :operatingsystem => 'Ubuntu',
+    :osfamily        => 'Debian',
+    :lsbdistid       => 'ubuntu',
+    :concat_basedir  => '/tmp'
+  } }
+  let(:title) { 'test_haproxy' }
+  context 'with defaults' do
+    let (:params) { { :name => 'testname', } }
+    it do
+      should_not contain_haproxy__listen('testname')
+      should_not contain_haproxy__balancermember('testname')
+    end
+  end
+
+  context 'with cluster_address, without listen_ports or balancer_ports' do
+    let (:params) { {
+      :name              => 'testname',
+      :cluster_addresses => ['10.1.1.1','10.1.1.2']
+    } }
+    it do
+      expect { should compile }.to raise_error(Puppet::Error,/Either balancer_ports or listen_ports must be provided/)
+    end
+  end
+
+  context 'with cluster_address, listen_ports' do
+    let (:params) { {
+      :name              => 'testname',
+      :cluster_addresses => ['10.1.1.1','10.1.1.2'],
+      :listen_ports      => 100,
+    } }
+
+    it do
+      should contain_haproxy__listen('testname').with({
+        'ipaddress' => '0.0.0.0',
+        'ports'     => '100',
+        'mode'      => 'tcp',
+        'collect_exported' => false,
+      })
+
+      should contain_haproxy__balancermember('testname').with({
+        'listening_service' => 'testname',
+        'ports'             => '100',
+        'server_names'      => ['10.1.1.1','10.1.1.2'],
+        'ipaddresses'       => ['10.1.1.1','10.1.1.2'],
+      })
+    end
+  end
+
+  context 'with cluster_address, vip, balancer_ports' do
+    let (:params) { {
+      :name => 'testname',
+      :cluster_addresses => ['10.1.1.1','10.1.1.2'],
+      :vip               => '10.1.1.100',
+      :balancer_ports    => 90,
+    } }
+    it do
+      should contain_haproxy__listen('testname').with({
+        'ipaddress' => '10.1.1.100',
+        'ports'     => '90',
+        'mode'      => 'tcp',
+        'collect_exported' => false,
+      })
+
+      should contain_haproxy__balancermember('testname').with({
+        'listening_service' => 'testname',
+        'ports'             => '90',
+        'server_names'      => ['10.1.1.1','10.1.1.2'],
+        'ipaddresses'       => ['10.1.1.1','10.1.1.2'],
+      })
+    end
+  end
+
+  context 'with cluster_address, vip, balancer_ports, listen_ports' do
+    let (:params) { {
+      :name => 'testname',
+      :cluster_addresses => ['10.1.1.1','10.1.1.2'],
+      :vip               => '10.1.1.100',
+      :balancer_ports    => 90,
+      :listen_ports      => 100,
+    } }
+    it do
+      should contain_haproxy__listen('testname').with({
+        'ipaddress' => '10.1.1.100',
+        'ports'     => '100',
+        'mode'      => 'tcp',
+        'collect_exported' => false,
+      })
+
+      should contain_haproxy__balancermember('testname').with({
+        'listening_service' => 'testname',
+        'ports'             => '90',
+        'server_names'      => ['10.1.1.1','10.1.1.2'],
+        'ipaddresses'       => ['10.1.1.1','10.1.1.2'],
+      })
+    end
+  end
+end
+
