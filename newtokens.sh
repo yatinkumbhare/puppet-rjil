f64ccf0050f616fbcfefb37fc79f365738efdd93
Soren Hansen <Soren.Hansen@ril.com>
2014-09-25 14:41:58 +0200

    Handle consul discovery token in Vagrantfile, too.

--- newtokens.sh
+++ newtokens.sh
@@ -1,4 +1,6 @@
-echo Fetching new token
+echo Fetching new tokens
+export consul_discovery_token=$(curl -s http://consuldiscovery.linux2go.dk/new)
+echo export consul_discovery_token=${consul_discovery_token}
 export etcd_discovery_token=$(curl -s https://discovery.etcd.io/new | awk -F/ '{ print $NF }')
 echo export etcd_discovery_token=${etcd_discovery_token}
-echo "(If you ran newtokens.sh with '. newtokens.sh' or 'source newtokens.sh', it has already been exported)"
+echo "(If you ran newtokens.sh with '. newtokens.sh' or 'source newtokens.sh', these have already been exported)"
