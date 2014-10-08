d23b86f654f5316ba8598d0b91f513e193fb5fba
Dan Bode <bodepd@gmail.com>
2014-10-07 21:52:11 -0700

    Added bind_addr param to consul classes
    Previously it only worked if you wanted to bind
    it to eth0 (or whatever happens to be the first
    interface).
    
    This patch adds a param to all consul classes so
    that it can be set.
    
    It also adds hiera data to set it for virtual-box.

--- manifests/jiocloud/consul/server.pp
+++ manifests/jiocloud/consul/server.pp
@@ -1,4 +1,6 @@
-class rjil::jiocloud::consul::server {
+class rjil::jiocloud::consul::server(
+  $bind_addr = '0.0.0.0'
+){
   if ($::consul_discovery_token) {
     $join_address = "${::consul_discovery_token}.service.consuldiscovery.linux2go.dk"
   } else {
@@ -7,6 +9,7 @@ class rjil::jiocloud::consul::server {
 
   class { 'rjil::jiocloud::consul':
     config_hash => {
+      'bind_addr'        => $bind_addr,
       'start_join'       => [$join_address],
       'datacenter'       => "$::env",
       'data_dir'         => '/var/lib/consul',
