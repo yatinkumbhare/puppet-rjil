3eeb8006bf450e06f6da1f548d25d63ff5b6f648
Soren Hansen <Soren.Hansen@ril.com>
2014-10-08 14:12:26 +0200

    Let Soren, Dan, Harish and Jenkins log in and sudo

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,12 +1,5 @@
 class rjil::base {
-  # install users
-  include rjil
   include rjil::jiocloud
   include rjil::system
-  realize (
-    Rjil::Localuser['jenkins'],
-    Rjil::Localuser['soren'],
-    Rjil::Localuser['bodepd'],
-  )
 }
 
