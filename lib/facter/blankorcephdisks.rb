d7bc06f8c094ea034d8fd199e535a5200734fb58
Soren Hansen <Soren.Hansen@ril.com>
2014-10-20 23:30:09 +0200

    Fix typo

--- lib/facter/blankorcephdisks.rb
+++ lib/facter/blankorcephdisks.rb
@@ -1,4 +1,4 @@
-Facter.add('blnkorcephdisks') do
+Facter.add('blankorcephdisks') do
   setcode do
     Facter.value(:blockdevices).split(/,/).select { |disk| Facter::Util::Resolution.exec("sudo parted -s /dev/#{disk} print | grep -A100 'Number.*Start.*End' | grep -P '^[\\s\\t]*[0-9]'| grep -vP '^[\\s\\t]+[0-9]+.*ceph'").empty? }.join(',')
   end
