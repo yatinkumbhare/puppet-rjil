33e9a42334014430856d5adb180846faba27a326
Soren Hansen <Soren.Hansen@ril.com>
2014-10-24 15:23:02 +0200

    Also allow passing mappings to cloudenv

--- manifests/jiocloud/jenkins/cloudenv.pp
+++ manifests/jiocloud/jenkins/cloudenv.pp
@@ -1,8 +1,15 @@
 define rjil::jiocloud::jenkins::cloudenv(
-  $vars = {}
+  $env_vars = {},
+  $mappings = {}
 ) {
   file { "/var/lib/jenkins/cloud.${name}.env":
     content => template("rjil/cloudenv.erb"),
+    owner => jenkins,
+    group => jenkins,
+    mode => '0600'
+  }
+  file { "/var/lib/jenkins/cloud.${name}.map.yaml":
+    content => template("rjil/cloudmap.erb"),
     owner => jenkins,
     group => jenkins,
     mode => '0600'
