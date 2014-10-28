5b9430317fa9fef73056192711d224f01755d1fb
Soren Hansen <Soren.Hansen@ril.com>
2014-10-28 13:16:56 +0100

    Create dns entry type
    It currently populates /etc/hosts and reloads dnsmasq.

---
+++ spec/defines/dns_entry_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe 'rjil::jiocloud::dns::entry' do
+  before(:each) do
+    MockFunction.new('dns_a') { |f|
+      f.stubs(:call).with(['some.other.host']).returns(['2.3.4.5', '5.6.7.8'])
+    }
+  end
+
+  let (:facts) { {
+    :operatingsystem => 'Ubuntu',
+    :osfamily        => 'Debian',
+    :lsbdistid       => 'ubuntu',
+    :concat_basedir  => '/tmp'
+  } }
+  let(:title) { 'somehost' }
+  context 'with defaults' do
+    let (:params) { { :name => 'testname', } }
+	it do
+      expect { should compile }.to raise_error(Puppet::Error,/neither IP nor CNAME/)
+	end
+  end
+  context 'with ip' do
+    let (:params) { { :name => 'testname',
+                      :ip => '1.2.3.4' } }
+	it do
+      should contain_host('testname').with({'ip' => '1.2.3.4'}).that_notifies("Class[Dnsmasq::Reload]")
+	end
+  end
+  context 'with cname' do
+    let (:params) { { :name => 'testname',
+                      :cname => 'some.other.host' } }
+	it do
+      should contain_host('testname').with({'ip' => '2.3.4.5'}).that_notifies("Class[Dnsmasq::Reload]")
+	end
+  end
+end
