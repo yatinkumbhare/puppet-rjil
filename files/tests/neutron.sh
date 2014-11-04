28eb370b9076dc5910dda90f2a9ef4fc882afd93
hkumarmk <hkumar@d4devops.org>
2014-11-05 02:00:28 +0530

    Fixed neutron test failures
    Neutron test was failing because the shared network was shown in net-list and
    the script tried to delete, but it did not as that network was of different
    tenant. Fixed it by only querying its own network by adding -D

--- files/tests/neutron.sh
+++ files/tests/neutron.sh
@@ -7,15 +7,15 @@ function fail {
 
 if [ -f /root/openrc ]; then
   source /root/openrc
-  neutron net-list || fail 'neutron net-list failed'
+  neutron net-list -D || fail 'neutron net-list failed'
   neutron net-create testnet || fail 'neutron net-create failed'
-  netid=`neutron net-list | awk '/[a-z][a-z]*[0-9][0-9]*/ {print $2}' | head -1`
+  netid=`neutron net-list -D | awk '/[a-z][a-z]*[0-9][0-9]*/ {print $2}' | head -1`
   neutron subnet-create $netid 10.0.0.0/24 || fail 'neutron subnet-create failed'
   neutron port-create $netid || fail 'neutron port-create failed'
   for port in `neutron port-list | awk '/[a-z][a-z]*[0-9][0-9]*/ {print $2}'`; do
     neutron port-delete $port || fail 'neutron port-delete failed'
   done
-  for net in `neutron net-list | awk '/[a-z][a-z]*[0-9][0-9]*/ {print $2}'`; do
+  for net in `neutron net-list -D  | awk '/[a-z][a-z]*[0-9][0-9]*/ {print $2}'`; do
     neutron net-delete $net || fail 'neutron net-delete failed'
   done
 else
