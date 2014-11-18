19f6ba707a3d3bdf41157a4fdbf784f3f61dc672
hkumarmk <hkumar@d4devops.org>
2014-11-18 14:47:33 +0530

    Added consul service for memcache.
    * Memcache should be a central service in order to use it to store tokens and to
      use multiple consoleauths. So adding a consul service for that.
    * added better test for memcached.
    * Added spec tests

--- files/tests/memcached.sh
+++ files/tests/memcached.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-set -e
-/usr/lib/nagios/plugins/check_procs -c 1:1 -C memcached
+
+if [ $# -gt 0 ]; then
+  case $1 in
+    -h|--help|help)
+      cat << __EOH__
+$0 - To check memcache status
+------------------------
+  This script check memcache status
+
+__EOH__
+       ;;
+   esac
+fi
+
+echo stats | nc localhost 11211 | grep uptime; rv=$?
+if [ $rv -eq 0 ]; then
+  echo "OK: Memcached is working fine"
+  exit 0
+else
+  echo "CRITICAL: Memcached is down"
+  exit 2
+fi
