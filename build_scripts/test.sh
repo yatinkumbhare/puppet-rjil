84dbac309f15d455802dd51a8b6a7ade1633aaad
Soren Hansen <Soren.Hansen@ril.com>
2015-01-14 03:09:27 +0530

    Tempest is installed on gcp1, not oc1

--- build_scripts/test.sh
+++ build_scripts/test.sh
@@ -4,4 +4,4 @@
 
 python -m jiocloud.apply_resources ssh_config --project_tag=${project_tag} ${mappings_arg} environment/${layout:-full}.yaml > ssh_config
 
-ssh -F ssh_config -l ${ssh_user:-jenkins} oc1_${project_tag} '~jenkins/tempest/run_tempest.sh -N' || true
+ssh -F ssh_config -l ${ssh_user:-jenkins} gcp1_${project_tag} '~jenkins/tempest/run_tempest.sh -N' || true
