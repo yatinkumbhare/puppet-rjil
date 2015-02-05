404a06c22224e6b3d98c70c22319657538424ce9
Soren Hansen <Soren.Hansen@ril.com>
2015-02-05 22:15:20 +0100

    Fix lint errors

--- manifests/default_manifest.pp
+++ manifests/default_manifest.pp
@@ -3,15 +3,15 @@
 #
 class rjil::default_manifest {
   if ($::settings::default_manifest == './manifests') {
-	$val = '/etc/puppet/manifests/site.pp'
+    $val = '/etc/puppet/manifests/site.pp'
   } else {
-	$val = "$::settings::default_manifest"
+    $val = $::settings::default_manifest
   }
 
-  ini_setting { default_manifest:
-    path    => "/etc/puppet/puppet.conf",
-	section => main,
-	setting => default_manifest,
-	value   => $val
+  ini_setting { 'default_manifest':
+    path    => '/etc/puppet/puppet.conf',
+    section => main,
+    setting => default_manifest,
+    value   => $val
   }
 }
