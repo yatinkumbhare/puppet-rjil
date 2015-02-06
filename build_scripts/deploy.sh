815f102f87a49853ab20b5e7d45ec8b98f607407
Soren Hansen <Soren.Hansen@ril.com>
2015-02-06 13:38:14 +0100

    In the unlikely event that they all work, don't fail

--- build_scripts/deploy.sh
+++ build_scripts/deploy.sh
@@ -16,7 +16,7 @@ time python -m jiocloud.apply_resources apply ${EXTRA_APPLY_RESOURCES_OPTS} --ke
 if [ "$cloud_provider" = 'hp' ]
 then
     sleep 100
-    nova list | grep test${BUILD_NUMBER} | cut -f2 -d' ' | while read uuid; do nova console-log $uuid | grep Giving.up.on.md && nova reboot $uuid; done
+    nova list | grep test${BUILD_NUMBER} | cut -f2 -d' ' | while read uuid; do nova console-log $uuid | grep Giving.up.on.md && nova reboot $uuid || true; done
 fi
 
 time $timeout 1200 bash -c 'while ! bash -c "ip=$(python -m jiocloud.utils get_ip_of_node ${consul_bootstrap_node:-bootstrap1}_${project_tag});ssh -o ServerAliveInterval=30 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \${ssh_user:-jenkins}@\${ip} python -m jiocloud.orchestrate ping"; do sleep 5; done'
