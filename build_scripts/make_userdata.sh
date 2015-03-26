ed7a1bb588ed1ae4a2edb0fc8e0f36bc7a431c1b
Soren Hansen <Soren.Hansen@ril.com>
2015-03-26 13:55:34 +0530

    internal.deb is now only available through proxy

--- build_scripts/make_userdata.sh
+++ build_scripts/make_userdata.sh
@@ -33,7 +33,7 @@ else
 fi
 wget -O jiocloud.deb -t 5 -T 30 \${jiocloud_repo_deb_url}
 dpkg -i puppet.deb jiocloud.deb
-if http_proxy= wget -t 2 -T 30 -O internal.deb http://apt.internal.jiocloud.com/internal.deb
+if no_proxy= wget -t 2 -T 30 -O internal.deb http://apt.internal.jiocloud.com/internal.deb
 then
   dpkg -i internal.deb
 fi
