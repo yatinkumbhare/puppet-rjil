f12f1f11f1bb33f02f96be2a2a91ba69276f1448
hkumarmk <hkumarmk@gmail.com>
2015-04-13 19:10:47 +0530

    Parameterized haproxy configs
    This will make the configuration changes to be easier.
    
    Also increased default timeout values to 20sec.

--- manifests/haproxy.pp
+++ manifests/haproxy.pp
@@ -5,37 +5,52 @@
 #
 #
 class rjil::haproxy (
-  $consul_service_tags = []
+  $consul_service_tags     = [],
+  $logfile                 = '/var/log/haproxy.log',
+  $log_level               = '127.0.0.1 local0 notice',
+  $default_log_level       = 'global',
+  $default_mode            = 'http',
+  $default_options         = ['httplog', 'dontlognull', 'redispatch'],
+  $default_retries         = 3,
+  $default_maxconn         = 5000,
+  $default_timeout_connect = 20000,
+  $default_timeout_client  = 20000,
+  $default_timeout_server  = 20000,
+  $errorfile               = [
+                              '400 /etc/haproxy/errors/400.http',
+                              '403 /etc/haproxy/errors/403.http',
+                              '408 /etc/haproxy/errors/408.http',
+                              '500 /etc/haproxy/errors/500.http',
+                              '502 /etc/haproxy/errors/502.http',
+                              '503 /etc/haproxy/errors/503.http',
+                              '504 /etc/haproxy/errors/504.http'
+                            ],
+  $global_maxconn          = 5000,
+  $stats                   = 'socket /var/run/haproxy mode 777',
 ) {
 
   rjil::test { 'haproxy.sh': }
 
   $haproxy_defaults = {
-    'log'        => 'global',
-    'mode'       => 'http',
-    'option'     => ['httplog', 'dontlognull', 'redispatch'],
-    'retries'    => '3',
-    'maxconn'    => '2000',
-    'timeout'    => ['connect 5000', 'client 10000', 'server 10000'],
-    'errorfile' => [
-                      '400 /etc/haproxy/errors/400.http',
-                      '403 /etc/haproxy/errors/403.http',
-                      '408 /etc/haproxy/errors/408.http',
-                      '500 /etc/haproxy/errors/500.http',
-                      '502 /etc/haproxy/errors/502.http',
-                      '503 /etc/haproxy/errors/503.http',
-                      '504 /etc/haproxy/errors/504.http'
-                    ],
+    'log'        => $default_log_level,
+    'mode'       => $default_mode,
+    'option'     => $default_options,
+    'retries'    => $default_retries,
+    'maxconn'    => $default_maxconn,
+    'timeout'    => [ "connect ${default_timeout_connect}",
+                      "client ${default_timeout_client}",
+                      "server ${default_timeout_server}" ],
+    'errorfile'  => $errorfile,
   }
 
   $haproxy_globals = {
-    'log'       => '127.0.0.1 local0 notice',
-    'maxconn'   => '4096',
+    'log'       => $log_level,
+    'maxconn'   => $global_maxconn,
     'user'      => 'haproxy',
     'group'     => 'haproxy',
     'daemon'    => '',
     'quiet'     => '',
-    'stats'     => 'socket /var/run/haproxy mode 777',
+    'stats'     => $stats,
   }
 
   class { '::haproxy':
@@ -70,46 +85,6 @@ class rjil::haproxy (
     tags          => $consul_service_tags
   }
 
-#  if $cluster_master == $::fqdn {
-#    $state = 'MASTER'
-#  } else {
-#    $state = 'BACKUP'
-#  }
-
-#  # configure vips
-#  include keepalived
-
-#  sysctl { 'net.ipv4.ip_nonlocal_bind':
-#    value  => '1',
-#    before => Class['::keepalived']
-#  }
-
-  # Since vswitch now moves ip addresses of interfaces to
-  # their attached ovs bridge, that bridge needs to be the
-  # public interface, and must be ready before keepalived starts
-#  Vs_port<||> -> Class['::keepalived']
-#  Vs_bridge<||> -> Class['::keepalived']
-
-#  keepalived::vrrp::instance { 'public':
-#    interface           => $public_iface,
-#    state               => $state,
-#    virtual_router_id   => $public_vrid,
-#    priority            => '101',
-#    auth_type           => 'PASS',
-#    auth_pass           => $vip_secret,
-#    virtual_ipaddress   => [$cluster_public_vip],
-#  } -> Class['::haproxy']
-
-#  keepalived::vrrp::instance { 'private':
-#    interface           => $private_iface,
-#    state               => $state,
-#    virtual_router_id   => $private_vrid,
-#    priority            => '101',
-#    auth_type           => 'PASS',
-#    auth_pass           => $vip_secret,
-#    virtual_ipaddress   => [$cluster_private_vip],
-#  } -> Class['::haproxy']
-
   # Openstack services depend on being able to access db and mq, so make
   # sure our VIPs and LB are active before we deal with them.
   Haproxy::Listen<||> -> Anchor <| title == 'mysql::server::start' |>
@@ -119,10 +94,4 @@ class rjil::haproxy (
   Service<| title == 'haproxy' |> -> Anchor <| title == 'rabbitmq::begin' |>
   Service<| title == 'haproxy' |> -> Anchor <| title == 'mysql::server::start' |>
 
-#  if $package_override {
-#    Package<| title == 'haproxy' |> {
-#      provider => 'rpm',
-#      source   => $package_override
-#    }
-#  }
 }
