63c60d4b7d5e7c48332833c43d3270a468157aab
hkumarmk <hkumarmk@gmail.com>
2015-04-17 17:41:19 +0530

    osd test should handle loop devices also
    This patch will make ceph test work for loop devices also where the partition
    have different naming convention.

--- templates/tests/ceph_osd.sh.rb
+++ templates/tests/ceph_osd.sh.rb
@@ -7,5 +7,9 @@ function fail {
   exit 2
 }
 
+<%- if @disk =~ /loop/ %>
+osdid=`df -h | grep "/dev/<%= @disk %>p[0-9]" | awk '{print $NF}' | cut -f2 -d-`
+<%- else %>
 osdid=`df -h | grep "/dev/<%= @disk %>[^a-z]" | awk '{print $NF}' | cut -f2 -d-`
+<%- end %>
 ps -efw | grep ceph-osd | grep osd.${osdid}.pid || fail "osd failed"
