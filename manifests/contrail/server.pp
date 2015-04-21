07d8938beb41c92c7a9f55a624d91db795df41e9
hkumarmk <hkumarmk@gmail.com>
2015-04-21 14:26:45 +0530

    Fix validation check failures

--- manifests/contrail/server.pp
+++ manifests/contrail/server.pp
@@ -1,16 +1,22 @@
 ###
 ## Class: rjil::contrail
 ###
-class rjil::contrail::server () {
+class rjil::contrail::server (
+  $enable_analytics = true,
+) {
 
   ##
   # Added tests
   ##
-  $contrail_tests = ['ifmap.sh','contrail-analytics.sh','contrail-api.sh',
+  $contrail_tests = ['ifmap.sh','contrail-api.sh',
                       'contrail-control.sh','contrail-discovery.sh',
                       'contrail-dns.sh','contrail-schema.sh',
                       'contrail-webui-webserver.sh','contrail-webui-jobserver.sh']
   rjil::test {$contrail_tests:}
+
+  if $enable_analytics {
+    rjil::test {'contrail-analytics.sh':}
+  }
 
   include ::contrail
 }
