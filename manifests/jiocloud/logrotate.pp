8aa4bf7f9b5083a6e53cd3c192c919e4c46d0c55
Anshu Prateek <anshu.prateek@ril.com>
2015-04-27 16:36:27 +0530

    formatting as per https://github.com/JioCloud/puppet-rjil/pull/570#discussion_r29131901

--- manifests/jiocloud/logrotate.pp
+++ manifests/jiocloud/logrotate.pp
@@ -1,11 +1,9 @@
 class rjil::jiocloud::logrotate {
   include logrotate
-  package {
-    logrotate:
+  package { logrotate:
     ensure => installed
   }
-  file {
-    '/etc/logrotate.conf':
+  file { '/etc/logrotate.conf':
       ensure  => file,
       mode    => '0444',
       source  => 'puppet:///modules/rjil/logrotate-default.conf';
