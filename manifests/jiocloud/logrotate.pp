53f789a33deb13437bf2855ea7417563d1c8fbe5
Anshu Prateek <anshu.prateek@ril.com>
2015-04-28 22:38:24 +0530

    changes per https://github.com/JioCloud/puppet-rjil/pull/574#discussion-diff-29239778 onwards

--- manifests/jiocloud/logrotate.pp
+++ manifests/jiocloud/logrotate.pp
@@ -1,18 +1,18 @@
 define rjil::jiocloud::logrotate(
   $service,
   $logfile,
-  $rotate_every = 'daily',
-  $rotate = 60,
-  $compress = true,
+  $rotate_every  = 'daily',
+  $rotate        = 60,
+  $compress      = true,
   $delaycompress = true,
-  $ifempty = false,
+  $ifempty       = false,
 ) {
-  logrotate::rule{$service:
-    path => $logfile,
-    rotate => $rotate,
-    rotate_every => $rotate_every,
-    compress => $compress,
+  logrotate::rule{ $service:
+    path          => $logfile,
+    rotate        => $rotate,
+    rotate_every  => $rotate_every,
+    compress      => $compress,
     delaycompress => $delaycompress,
-    ifempty => $ifempty
-    }
+    ifempty       => $ifempty
   }
+}
