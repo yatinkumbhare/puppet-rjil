ece7f5610d3989efd13beba9e46f784f8f730257
hkumarmk <hkumarmk@gmail.com>
2015-06-10 17:12:05 +0530

    Use existing ssh_config if possible
    debug code from deploy.sh is already creating ssh_config, so no ned to recreate
    it. Also it was using same ssh_config, which is also changed.

--- build_scripts/test.sh
+++ build_scripts/test.sh
@@ -2,6 +2,8 @@
 
 . $(dirname $0)/common.sh
 
-python -m jiocloud.apply_resources ssh_config --project_tag=${project_tag} ${mappings_arg} environment/${layout:-full}.yaml > ssh_config
+if [ ! -f ${project_tag}_ssh_config ]; then
+  python -m jiocloud.apply_resources ssh_config --project_tag=${project_tag} ${mappings_arg} environment/${layout:-full}.yaml > ${project_tag}_ssh_config
+fi
 
 ssh -F ssh_config -l ${ssh_user:-jenkins} gcp1_${project_tag} '~jenkins/tempest/run_tempest.sh -N -- --load-list ~jenkins/tempest_tests.txt'
