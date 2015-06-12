4fbc134a4104956b00ad0e6529790b88ec97acf2
Dan Bode <bodepd@gmail.com>
2015-06-12 11:00:36 -0600

    updates to consul-alerts patch
    * change the default config so that it
      is not enabled by default.
    * switch to using package
    * update path to match package
    * switch from create resources to regular resource
    * ensure that consul kv updates cause a service restart
    * allow slack_url to be set from a fact

--- manifests/jiocloud/consul/consul_alerts.pp
+++ manifests/jiocloud/consul/consul_alerts.pp
@@ -5,64 +5,50 @@
 #
 
 class rjil::jiocloud::consul::consul_alerts (
-  $health_check           = 'false',
+  $slack_url              = $::slack_url,
   $check_thresold         = '30',
-  $slack_notifier_enabled = 'true',
-  $slack_enabled          = 'true',
   $slack_cluster_name     = 'consul-alerts',
-  $slack_url              = 'https://hooks.slack.com/services',
   $slack_username         = 'WatchBot',
   $slack_channel          = 'consul-alerts',
-  $bin_dir                = '/usr/local/sbin',
-  $download_url           = 'https://bintray.com/artifact/download/darkcrux/generic/consul-alerts-latest-linux-amd64.tar',
+  $bin_dir                = '/usr/bin',
 ) {
 
-  staging::file { 'consul-alerts.tar':
-    source => $download_url,
-    target   => '/tmp/consul-alerts.tar',
-  } ->
-  exec { "tar -xf /tmp/consul-alerts.tar":
-    cwd     => "$bin_dir",
-    creates => "$bin_dir/consul-alerts",
-    path    => ["/usr/bin", "/usr/sbin", "/bin"]
-  } ->
-  file { "$bin_dir/consul-alerts":
-    owner => 'root',
-    group => 0,
-    mode  => '0555',
+  package { 'consul-alerts':
+    ensure => present,
   }
 
-  $healthcheck = {
-    'consul-alerts/config/checks/enabled'        => { value => $health_check, },
-    'consul-alerts/config/checks/check_thresold' => { value => $check_thresold, },
+  Consul_kv {
+    notify => Service['consul-alerts']
   }
 
-  $slack_notifier = {
-    'consul-alerts/config/notifiers/slack/enabled'      => { value => $slack_notifier_enabled, },
-    'consul-alerts/config/notifiers/slack/cluster_name' => { value => $slack_cluster_name, },
-    'consul-alerts/config/notifiers/slack/url'          => { value => $slack_url, },
-    'consul-alerts/config/notifiers/slack/username'     => { value => $slack_username, },
-    'consul-alerts/config/notifiers/slack/channel'      => { value => $slack_channel, },
+  if $slack_url {
+    $enabled = 'true'
+    consul_kv {
+      'consul-alerts/config/notifiers/slack/url':          value => $slack_url;
+    }
+  } else {
+    $enabled = 'false'
   }
-
-  create_resources(consul_kv, $healthcheck)
-
-  if $slack_notifier_enabled {
-    create_resources(consul_kv, $slack_notifier)
+  consul_kv {
+    'consul-alerts/config/checks/enabled':               value => $enabled;
+    'consul-alerts/config/notifiers/slack/enabled':      value => $enabled;
+    'consul-alerts/config/checks/check_thresold':        value => $check_thresold;
+    'consul-alerts/config/notifiers/slack/cluster_name': value => $slack_cluster_name;
+    'consul-alerts/config/notifiers/slack/username':     value => $slack_username;
+    'consul-alerts/config/notifiers/slack/channel':      value => $slack_channel;
   }
 
   file { '/etc/init/consul-alerts.conf':
     owner   => 'root',
     group   => 'root',
     mode    => '0444',
-    require => File["$bin_dir/consul-alerts"],
+    require => Package['consul-alerts'],
     content => template('rjil/consul-alerts.erb'),
     notify  => Service[consul-alerts]
   }
 
   service {'consul-alerts':
     ensure  => 'running',
-    require => File['/etc/init/consul-alerts.conf'],
   }
 
 }
