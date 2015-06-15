33974f4c46981339a6ba6b29701c5905682d4cb0
Anshu Prateek <anshu.prateek@ril.com>
2015-06-15 17:43:17 +0530

    changed check logic per review

--- files/tests/contrail-vrouter-check.sh
+++ files/tests/contrail-vrouter-check.sh
@@ -4,13 +4,13 @@ log_file="/var/log/contrail/rjil-vrouter-restart.log"
 test_cmd='curl -s -m 5 -o /dev/null http://localhost:8085/Snh_ItfReq?name='
 lock_file='/var/run/contrail-vrouter-restart.err.lock'
 
-if [ ! -f $lock_file ]
+flag_warning=0
+last_step='Curl'
+${test_cmd}
+rv=$?
+if [ $rv == 28 ]
 then
-    flag_warning=0
-    last_step='Curl'
-    ${test_cmd}
-    rv=$?
-    if [ $rv == 28 ]
+    if [ ! -f $lock_file ]
     then
         echo "[INFO] `date` `hostname` [`date +%s`] Restarting vrouter ">>${log_file}
         last_step='Agent restart'
@@ -19,14 +19,24 @@ then
         sleep 5
         ${test_cmd}
         rv=$?
+    else
+        exit 1
     fi
-    if [ $rv != 0 ]
-    then
-        echo "[ERROR] `date` `hostname` [`date +%s`] ${last_step} failure - code ${rv}">>${log_file}
-        echo "[ERROR] `date`" >>${lock_file}
-        exit 2
-    fi
-    exit $flag_warning
-else
-    exit 1
 fi
+if [ $rv != 0 ]
+then
+    echo "[ERROR] `date` `hostname` [`date +%s`] ${last_step} failure - code ${rv}">>${log_file}
+    echo "[ERROR] `date`" >>${lock_file}
+    exit 2
+fi
+##  
+# If we have reached here, then the check is ok or warning. We dont need the
+# lock file as the check is not failed.
+##
+
+if [ -f $lock_file ]
+then
+    rm $lock_file
+fi
+
+exit $flag_warning
