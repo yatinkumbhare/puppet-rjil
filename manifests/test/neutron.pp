dc392419493f53ebfa13d017b14e580b956be985
hkumarmk <hkumarmk@gmail.com>
2015-06-15 10:19:56 +0530

    Params to disable tests that are not possible for undercloud
    Provided params to rjil::test::neutron to diabled the tests to create networks
    and fips as they are not possible to be run for undercloud.

--- manifests/test/neutron.pp
+++ manifests/test/neutron.pp
@@ -2,8 +2,20 @@
 # Class: rjil::test::neutron
 #   Adding tests for neutron services
 #
+# == Parameters
+#
+# [*fip_available*]
+#   This tell whether fip is available for the cloud or not, if not available
+#   do not test them.
+#
+# [*test_netcreate*]
+#   whether to test network/subnet creation - this may not be possible for
+#   undercloud.
+#
 
 class rjil::test::neutron(
+  $fip_available  = true,
+  $test_netcreate = true
 ) {
 
   include openstack_extras::auth_file
@@ -11,16 +23,18 @@ class rjil::test::neutron(
   include rjil::test::base
 
   file { '/usr/lib/jiocloud/tests/neutron-service.sh':
-    source => 'puppet:///modules/rjil/tests/neutron.sh',
+    content => template("rjil/tests/neutron.sh.erb"),
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
   }
 
-  file { '/usr/lib/jiocloud/tests/floating_ip.sh':
-    source => 'puppet:///modules/rjil/tests/floating_ip.sh',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
+  if $fip_available {
+    file { '/usr/lib/jiocloud/tests/floating_ip.sh':
+      source => 'puppet:///modules/rjil/tests/floating_ip.sh',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+    }
   }
 }
