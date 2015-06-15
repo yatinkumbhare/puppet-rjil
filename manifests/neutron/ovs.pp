5e40a6c4b2c41a6394f396afb63d85ee78e0d9a7
hkumarmk <hkumarmk@gmail.com>
2015-06-15 09:53:50 +0530

    Made the params more generic and added more appropriate params
    Added more generic params for rjil::neutron::ovs. Also renamed the template to
    .erb.
    also added code to automatically detect reasonalble defaults if not provided.

--- manifests/neutron/ovs.pp
+++ manifests/neutron/ovs.pp
@@ -3,12 +3,58 @@
 #
 
 class rjil::neutron::ovs(
-  $ctlplane_network_name       = 'ctlplane',
-  $ctlplane_physical_interface = 'eth0',
-  $ctlplane_nameservers        = '8.8.8.8',
-  $ctlplane_domain             = 'undercloud.local',
-  $l3_agent_enabled            = false,
+  $gateway,
+  $br_address            = undef,
+  $br_netmask            = undef,
+  $br_network            = undef,
+  $br_name               = 'br-ctlplane',
+  $br_physical_interface = 'eth0',
+  $nameservers           = undef,
+  $domain                = 'openstack.local',
+  $l3_agent_enabled      = false,
 ) {
+
+  $br_name_for_facter = regsubst($br_name,'-','_','G')
+
+  if ! $br_address {
+    $ipaddr_phy_iface = inline_template("<%= scope.lookupvar('ipaddress_' + @br_physical_interface) %>")
+    $ipaddr_br_iface  = inline_template("<%= scope.lookupvar('ipaddress_' + @br_name_for_facter) %>")
+
+    if $ipaddr_br_iface {
+      $br_address_orig = $ipaddr_br_iface
+    } elsif $ipaddr_phy_iface {
+      $br_address_orig = $ipaddr_phy_iface
+    }
+  } else {
+    $br_address_orig = $br_address
+  }
+
+  if ! $br_netmask {
+    $netmask_phy_iface = inline_template("<%= scope.lookupvar('netmask_' + @br_physical_interface) %>")
+    $netmask_br_iface  = inline_template("<%= scope.lookupvar('netmask_' + @br_name_for_facter) %>")
+
+    if $netmask_br_iface {
+      $br_netmask_orig = $netmask_br_iface
+    } elsif $netmask_phy_iface {
+      $br_netmask_orig = $netmask_phy_iface
+    }
+  } else {
+    $br_netmask_orig = $br_netmask
+  }
+
+
+  if ! $br_network {
+    $network_phy_iface = inline_template("<%= scope.lookupvar('network_' + @br_physical_interface) %>")
+    $network_br_iface  = inline_template("<%= scope.lookupvar('network_' + @br_name_for_facter) %>")
+
+    if $network_br_iface {
+      $br_network_orig = $network_br_iface
+    } elsif $network_phy_iface {
+      $br_network_orig = $network_phy_iface
+    }
+  } else {
+    $br_network_orig = $br_network
+  }
 
   include ::rjil::neutron
 
@@ -40,7 +86,7 @@ class rjil::neutron::ovs(
     bridge => "br-${ctlplane_network_name}",
   } ->
   file { '/etc/network/interfaces.new':
-    content => template('rjil/undercloud_etc_network_interfaces.tmpl'),
+    content => template('rjil/undercloud_etc_network_interfaces.erb'),
     notify => Exec['network-down']
   } ~>
   exec { 'network-down':
