79a77c09c6010a6b2c2e7beaecbf84d2884f19b3
hkumarmk <hkumarmk@gmail.com>
2015-06-17 14:45:44 +0530

    rjil::test::check changes
    This patch:
    * Added a parameter to rjil::test::check to use it for both service and
     validation checks
    * Added a udp port check script template using netcat - nagios check_udp doesnt
     work as expected

--- manifests/test/check.pp
+++ manifests/test/check.pp
@@ -4,15 +4,24 @@
 #
 
 define rjil::test::check(
-  $port    = 0,
-  $address = '127.0.0.1',
-  $ssl     = false,
-  $type    = 'http',
+  $port       = 0,
+  $address    = '127.0.0.1',
+  $ssl        = false,
+  $type       = 'http',
+  $check_type = 'service',
 ) {
 
   include rjil::test::base
 
-  file { "/usr/lib/jiocloud/tests/service_checks/${name}.sh":
+  if $check_type == 'service' {
+    $test_file = "/usr/lib/jiocloud/tests/service_checks/${name}.sh"
+  } elsif $check_type == 'validation' {
+    $test_file = "/usr/lib/jiocloud/tests/${name}.sh"
+  } else {
+    fail("Invalid check_type - $check_type)")
+  }
+
+  file { $test_file:
     content => template("rjil/tests/${type}_check.sh.erb"),
     owner   => 'root',
     group   => 'root',
