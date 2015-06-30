fa07af0a1a1bab593c15b02abc52538fe352b06e
hkumarmk <hkumarmk@gmail.com>
2015-06-30 09:22:06 +0530

    Explicitely fail if private/public address/interface is nil
    New kind of ipaddress and interface facts (ipaddress and interface based on
    subnet assigned to them) are raising new problem - in case of a misconfiguration
    of subnets, those facts can return null values for those facts. In that case,
    puppet run should explicitely fail to avoid the system to go into invalid state.
    
    NOTE: This code assume the fact is only used for private_address, public_address,
    private_interface, public_interface. If in future those facts used for anything
    else, they must be validated either here or in appropriate code.

--- manifests/base.pp
+++ manifests/base.pp
@@ -18,5 +18,28 @@ class rjil::base (
   if $self_signed_cert {
     include rjil::trust_selfsigned_cert
   }
+
+  ##
+  # New kind of ipaddress and interface facts (ipaddress and interface based on
+  # subnet assigned to them) are raising new problem - in case
+  # of a misconfiguration of subnets, those facts can return null values for
+  # those facts. In that case, puppet run should explicitely fail to avoid the
+  # system to go into invalid state.
+  # NOTE: This code assume the fact is only used for private_address,
+  # public_address, private_interface, public_interface. If in future those
+  # facts used for anything else, they must be validated either here or in
+  # appropriate code.
+  ##
+
+  if ! hiera('private_address') {
+    fail("hiera data for 'private_address' is not known")
+  } elsif ! hiera('private_interface') {
+    fail("Hiera data for 'private_interface' is not known")
+  } elsif ! hiera('public_address') {
+    fail("hiera data for 'public_address' is not known")
+  } elsif ! hiera('public_interface') {
+    fail("Hiera data for 'public_interface' is not known")
+  }
+
 }
 
